f256a53fb35df7cbe277835268188391963dd779
Pui Yong Qing <puiyongqing@gmail.com>
2025-12-07 20:53:48 +0800

    gamescope: disable as crash, gamemode: fix gpu device number

--- modules/core/steam.nix
+++ modules/core/steam.nix
@@ -5,7 +5,7 @@
       enable = true;
       remotePlay.openFirewall = false;
       dedicatedServer.openFirewall = false;
-      gamescopeSession.enable = true;
+      #gamescopeSession.enable = true;
       protontricks.enable = false;
       extraCompatPackages = [
         # pkgs.proton-ge-custom
@@ -13,20 +13,20 @@
       ];
     };
 
-    gamescope = {
-      enable = true;
-      capSysNice = true;
-      args = [
-        "--rt"
-        "--expose-wayland"
-      ];
-    };
+    #gamescope = {
+    #enable = true;
+    # capSysNice = true;
+    #args = [
+    #   "--rt"
+    #   "--expose-wayland"
+    # ];
+    #};
 
     gamemode = {
       enable = true;
       settings.gpu = {
         apply_gpu_optimisations = "accept-responsibility";
-        gpu_device = 0;
+        gpu_device = 1;
         amd_performance_level = "high";
       };
     };
