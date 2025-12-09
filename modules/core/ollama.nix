5cd6b34b28f61f6983c784351178b8217ff8b67f
Pui Yong Qing <puiyongqing@gmail.com>
2025-12-09 17:49:54 +0800

    ollama: refactor generally cpu is faster than igpu acceleration that not that strong as dgpu, even with vulkan

--- modules/core/ollama.nix
+++ modules/core/ollama.nix
@@ -1,13 +1,22 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 {
   services = {
     open-webui.enable = true;
     ollama = {
       enable = true;
+    }
+    // lib.optionalAttrs config.rocmSupport {
       package = pkgs.ollama-rocm;
       acceleration = "rocm";
       rocmOverrideGfx = "11.0.0";
     };
   };
-  boot.kernelModules = [ "amdgpu" ]; # workaround of race condition (see issue #422355 on nixpkgs)
+  boot.kernelModules = (lib.mkIf config.services.ollama.acceleration == "rocm" || "vulkan") [
+    "amdgpu"
+  ]; # workaround of race condition (see issue #422355 on nixpkgs)
 }
