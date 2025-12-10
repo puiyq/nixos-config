fd42a70152955f808f1ebdb3376c24b19a917a99
Pui Yong Qing <puiyongqing@gmail.com>
2025-12-11 03:14:55 +0800

    ci: prepare for garnix build cache

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -32,10 +32,12 @@ in
       substituters = [
         "https://nix-community.cachix.org"
         "https://puiyq.cachix.org"
+        "https://cache.garnix.io"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "puiyq.cachix.org-1:x3l4E/KXWxCSELeZlxB52NVOfof240vPjIZUEQp5RHw="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
       trusted-users = [
         "@wheel"
