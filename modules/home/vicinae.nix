0525be5c2da50169719f196e47837d8681161bd1
Pui Yong Qing <puiyongqing@gmail.com>
2025-12-28 01:18:47 +0800

    vicinae: add extension

--- modules/home/vicinae.nix
+++ modules/home/vicinae.nix
@@ -6,6 +6,7 @@
     systemd.enable = true;
     extensions = with inputs.vicinae-extensions.packages.${pkgs.stdenv.hostPlatform.system}; [
       nix
+      power-profile
       hypr-keybinds
     ];
     settings = {
