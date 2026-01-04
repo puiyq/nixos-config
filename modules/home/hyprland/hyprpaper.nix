dae99ff76c080f06b164a4cc9d20996079e5f91a
Pui Yong Qing <puiyongqing@gmail.com>
2026-01-05 04:31:32 +0800

    hyprpaper: temp manually change to new api

--- modules/home/hyprland/hyprpaper.nix
+++ modules/home/hyprland/hyprpaper.nix
@@ -1,13 +1,23 @@
 {
   services.hyprpaper = {
     enable = true;
-    settings = {
-      preload = [
-        "~/Pictures/Wallpapers/AnimeGirlNightSky.jpg"
-      ];
-      wallpaper = [
-        ",~/Pictures/Wallpapers/AnimeGirlNightSky.jpg"
-      ];
+    #settings = ''
+    # wallpaper {
+    #    path = ~/Pictures/Wallpapers/AnimeGirlNightSky.jpg
+    #}
+    #'';
+  };
+  home.file = {
+    ".config/hypr/hyprpaper.conf" = {
+      text = ''
+        splash = false
+
+        wallpaper {
+            monitor = eDP-1
+            path = ~/Pictures/Wallpapers/AnimeGirlNightSky.jpg
+            fit_mode = cover
+        }
+      '';
     };
   };
 }
