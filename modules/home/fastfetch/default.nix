7413254807b524b964da6f5569033ea2527510dd
Pui Yong Qing <puiyongqing@gmail.com>
2026-01-09 13:16:22 +0800

    fastfetch: config update

--- modules/home/fastfetch/default.nix
+++ modules/home/fastfetch/default.nix
@@ -5,92 +5,125 @@
     settings = {
       "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
       display = {
-        color = "magenta";
-        separator = "    ";
+        color = {
+          keys = "35";
+          output = "95";
+        };
+        separator = " ➜  ";
       };
-      logo.source = ./icon.png;
+
+      logo = {
+        source = ./icon.png;
+        type = "kitty-direct";
+        height = 10;
+        width = 20;
+        padding = {
+          top = 2;
+          left = 2;
+        };
+      };
+
       modules = [
-        {
-          type = "custom";
-          format = " System";
-        }
-        {
-          type = "title";
-          key = " ";
-        }
+        "break"
         {
           type = "os";
-          key = " 󱄅";
-          format = "{3}";
+          key = "OS";
+          keyColor = "31";
         }
         {
           type = "kernel";
-          key = " ";
+          key = " ├  ";
+          keyColor = "31";
         }
         {
           type = "packages";
-          key = " 󰏗";
-        }
-        {
-          type = "custom";
-          format = " ────────────────────────────────";
-        }
-        {
-          type = "custom";
-          format = " Hardware";
-        }
-        {
-          type = "cpu";
-          key = " ";
-          format = "{}";
-        }
-        {
-          type = "gpu";
-          key = " ";
-          format = "{1}, {3}";
-        }
-        {
-          type = "memory";
-          key = " ";
-          format = "{1} / {2}";
-        }
-        {
-          type = "disk";
-          key = " ";
-          format = "{1} / {2}";
-        }
-        {
-          type = "custom";
-          format = " ────────────────────────────────";
-        }
-        {
-          type = "custom";
-          format = " Profile";
-        }
-        {
-          type = "wm";
-          key = " 󰖯";
-          format = "{}";
-        }
-        {
-          type = "terminal";
-          key = " ";
+          key = " ├ 󰏖 ";
+          keyColor = "31";
         }
         {
           type = "shell";
-          key = " ";
+          key = " └  ";
+          keyColor = "31";
         }
+        "break"
+        {
+          type = "wm";
+          key = "WM   ";
+          keyColor = "32";
+        }
+        {
+          type = "wmtheme";
+          key = " ├ 󰉼 ";
+          keyColor = "32";
+        }
+        {
+          type = "icons";
+          key = " ├ 󰀻 ";
+          keyColor = "32";
+        }
+        {
+          type = "cursor";
+          key = " ├  ";
+          keyColor = "32";
+        }
+        {
+          type = "terminal";
+          key = " ├  ";
+          keyColor = "32";
+        }
+        {
+          type = "terminalfont";
+          key = " └  ";
+          keyColor = "32";
+        }
+        "break"
+        {
+          type = "host";
+          format = "{5} {1} Type {2}";
+          key = "PC   ";
+          keyColor = "33";
+        }
+        {
+          type = "cpu";
+          format = "{1} ({3}) @ {7}";
+          key = " ├  ";
+          keyColor = "33";
+        }
+        {
+          type = "gpu";
+          format = "{1} {2} @ {12} GHz";
+          key = " ├ 󰢮 ";
+          keyColor = "33";
+        }
+        {
+          type = "memory";
+          key = " ├  ";
+          keyColor = "33";
+        }
+        {
+          type = "disk";
+          key = " ├ 󰋊 ";
+          keyColor = "33";
+        }
+        {
+          type = "monitor";
+          key = " ├  ";
+          keyColor = "33";
+        }
+        {
+          type = "player";
+          key = " ├ 󰥠 ";
+          keyColor = "33";
+        }
+        {
+          type = "media";
+          key = " └ 󰝚 ";
+          keyColor = "33";
+        }
+        "break"
         {
           type = "uptime";
-          key = " 󰥔";
-        }
-        {
-          type = "custom";
-          format = " ────────────────────────────────";
-        }
-        {
-          type = "custom";
-          format = " {#30}⬤ {#90}⬤ {#31}⬤ {#91}⬤ {#32}⬤ {#92}⬤ {#33}⬤ {#93}⬤ {#34}⬤ {#94}⬤ {#35}⬤ {#95}⬤ {#36}⬤ {#96}⬤ {#37}⬤ {#97}⬤ ";
+          key = "   Uptime   ";
         }
       ];
     };
