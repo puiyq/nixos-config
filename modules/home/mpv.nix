70ff74c32458d786eff603fe6e703524c92d3453
Pui Yong Qing <puiyongqing@gmail.com>
2026-01-09 13:16:22 +0800

    mpv: update config

--- modules/home/mpv.nix
+++ modules/home/mpv.nix
@@ -2,12 +2,52 @@
 {
   programs.mpv = {
     enable = true;
-    scripts = [ pkgs.mpvScripts.bdanmaku ];
+    package = pkgs.mpv.override {
+      mpv-unwrapped = pkgs.mpv-unwrapped.override {
+        nv-codec-headers-11 = null;
+        alsaSupport = false;
+        archiveSupport = false;
+        bluraySupport = false;
+        bs2bSupport = false;
+        cacaSupport = false;
+        cmsSupport = false;
+        dvbinSupport = false;
+        dvdnavSupport = false;
+        javascriptSupport = false;
+        openalSupport = false;
+        pulseSupport = false;
+        rubberbandSupport = false;
+        vdpauSupport = false;
+        x11Support = false;
+        zimgSupport = false;
+      };
+      youtubeSupport = false;
+      scripts = with pkgs.mpvScripts; [
+        bdanmaku
+      ];
+    };
     defaultProfiles = [ "gpu-hq" ];
     config = {
       hwdec = "auto";
       ytdl-format = "bestvideo+bestaudio";
-      cache-default = 4000000;
+      audio-device = "pipewire";
+      gpu-api = "vulkan";
+      hwdec-codecs = "all";
+      deband = "yes";
+      scale = "ewa_lanczossharp";
+      cscale = "ewa_lanczossharp";
+      dscale = "mitchell";
+      temporal-dither = "yes";
+      video-sync = "display-resample";
+      af = "lavfi=[aresample=resampler=soxr]";
+    };
+  };
+  programs.yt-dlp = {
+    enable = true;
+    package = pkgs.yt-dlp-light;
+    settings = {
+      downloader = "aria2c";
+      downloader-args = "aria2c:'-c -x16 -s16 -k1M'";
     };
   };
 }
