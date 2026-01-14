c92fc710e81185e2a796c13c1d2fb649626b52d6
Pui Yong Qing <puiyongqing@gmail.com>
2026-01-14 14:25:56 +0800

    power management: power-profile-daemon -> tlp

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -55,7 +55,17 @@
       package = pkgs.scx.rustscheds;
     };
     gnome.gnome-keyring.enable = true;
-    power-profiles-daemon.enable = true;
+    tlp = {
+      enable = true;
+      pd.enable = true;
+      settings = {
+        START_CHARGE_THRESH_BAT0 = 0;
+        STOP_CHARGE_THRESH_BAT0 = 80;
+        SOUND_POWER_SAVE_ON_AC = 10;
+        SOUND_POWER_SAVE_ON_BAT = 10;
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      };
+    };
     pipewire = {
       enable = true;
       extraConfig.pipewire = {
