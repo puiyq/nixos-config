ec6fec0fd29f9ad72a15bb4640121673fccdb237
Pui Yong Qing <puiyongqing@gmail.com>
2026-01-19 21:23:24 +0800

    fonts: clean up

--- modules/core/fonts.nix
+++ modules/core/fonts.nix
@@ -2,14 +2,8 @@
 {
   fonts = {
     packages = with pkgs; [
-      noto-fonts-color-emoji
       noto-fonts-cjk-sans
-      font-awesome
-      #symbola  Fails to build 7/16/25 waiting on fix
-      # https://github.com/nixos/nixpkgs/issues/425166
-      material-icons
-      fira-code
-      fira-code-symbols
+      noto-fonts-cjk-serif
       corefonts
     ];
   };
