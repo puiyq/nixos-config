c7e565b1c0daf956880ac37c0ee0e0f05c096ab7
Pui Yong Qing <puiyongqing@gmail.com>
2026-01-23 23:04:33 +0800

    nix: using default package

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -1,17 +1,12 @@
 {
-  pkgs,
-  inputs,
-  ...
-}:
-{
   nix = {
     daemonCPUSchedPolicy = "idle";
     daemonIOSchedClass = "idle";
     channel.enable = false;
-    package = inputs.determinate.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    #package =  inputs.determinate.packages.${pkgs.stdenv.hostPlatform.system}.default;
     settings = {
-      eval-cores = 0;
-      lazy-trees = true;
+      #eval-cores = 0;
+      #lazy-trees = true;
       download-buffer-size = 250000000;
       use-xdg-base-directories = true;
       auto-allocate-uids = true;
