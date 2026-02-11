9ac4025ef138f325a03a268e7ac3ef010c22cb9e
Pui Yong Qing <puiyongqing@gmail.com>
2026-02-12 00:21:49 +0800

    network: using iwd as networkmanager's backend

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -2,10 +2,13 @@
 {
   networking = {
     hostName = host;
+
     networkmanager = {
       enable = true;
       dns = "systemd-resolved";
+      wifi.backend = "iwd";
     };
+
     nftables.enable = true;
     firewall = {
       enable = true;
