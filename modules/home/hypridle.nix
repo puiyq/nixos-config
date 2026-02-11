2394afc119f495ff94f4ca3a2ec2e7e75a7c7ade
Pui Yong Qing <puiyongqing@gmail.com>
2026-02-11 23:29:22 +0800

    hypridle: update config

--- modules/home/hypridle.nix
+++ modules/home/hypridle.nix
@@ -5,22 +5,27 @@
       settings = {
         general = {
           ignore_dbus_inhibit = false;
-          lock_cmd = "noctalia ipc call lockScreen lock";
+          lock_cmd = "noctalia-shell ipc call lockScreen lock";
           before_sleep_cmd = "loginctl lock-session";
         };
 
         listener = [
           {
-            timeout = 180; # 3 minutes
-            on-timeout = "brightnessctl --save --device=asus::kbd_backlight set 0"; # turn off keyboard backlight
-            on-resume = "brightnessctl --restore --device=asus::kbd_backlight"; # turn on keyboard backlight
+            timeout = 180; # 3min: turn off keyboard backlight
+            on-timeout = "brightnessctl --save --device=asus::kbd_backlight set 0";
+            on-resume = "brightnessctl --restore --device=asus::kbd_backlight";
           }
           {
-            timeout = 900; # 15 minutes
-            on-timeout = "noctalia ipc call lockScreen lock";
+            timeout = 600; # 10min: dim screen as warning
+            on-timeout = "brightnessctl --save --device=amdgpu_bl1 set 30%";
+            on-resume = "brightnessctl --restore --device=amdgpu_bl1";
           }
           {
-            timeout = 1200; # 20 minutes
+            timeout = 720; # 12min: lock session
+            on-timeout = "loginctl lock-session";
+          }
+          {
+            timeout = 1800; # 30min: suspend
             on-timeout = "systemctl suspend";
           }
         ];
