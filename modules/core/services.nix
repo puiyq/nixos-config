9a8b01ea89baa70b7023d9de3222a29499de712e
Pui Yong Qing <puiyongqing@gmail.com>
2026-02-12 04:33:58 +0800

    scx: add args

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -93,6 +93,10 @@
       enable = true; # by default uses scx_rustland scheduler
       scheduler = "scx_rusty";
       package = pkgs.scx.rustscheds;
+      extraArgs = [
+        "--balanced-kworkers" # let kernel handle kworker balancing (>=6.6), avoid conflicting with scx
+        "--kthreads-local" # pin per-cpu kthreads to local dsq for lower latency
+      ];
     };
     gnome.gnome-keyring.enable = true;
     logind.settings.Login = {
