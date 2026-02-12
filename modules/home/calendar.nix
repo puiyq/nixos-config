d1f923dd7ae125c4478b3396f0c44ce4dedbdf8a
Pui Yong Qing <puiyongqing@gmail.com>
2026-02-13 05:27:58 +0800

    calendar: remove holiday calendar

--- modules/home/calendar.nix
+++ modules/home/calendar.nix
@@ -1,73 +1,44 @@
 { config, osConfig, ... }:
-let
-  # Common settings
-  commonVdirsyncerSettings = {
-    enable = true;
-    metadata = [
-      "color"
-      "displayname"
-    ];
-    tokenFile = "${config.xdg.dataHome}/vdirsyncer/google_token";
-    clientIdCommand = [
-      "cat"
-      osConfig.sops.secrets."token/google/calendar_client_id".path
-    ];
-    clientSecretCommand = [
-      "cat"
-      osConfig.sops.secrets."token/google/calendar_client_secret".path
-    ];
-    timeRange = {
-      start = "datetime.now() - timedelta(days=90)";
-      end = "datetime.now() + timedelta(days=365)";
-    };
-  };
-
-  commonLocalSettings = {
-    type = "filesystem";
-    fileExt = ".ics";
-  };
-
-in
 {
   accounts.calendar = {
     basePath = "${config.xdg.dataHome}/calendars";
 
-    accounts = {
-      google = {
-        remote.type = "google_calendar";
-        local = commonLocalSettings;
-
-        khal = {
-          enable = true;
-          type = "discover";
-          readOnly = false;
-        };
-
-        vdirsyncer = commonVdirsyncerSettings // {
-          collections = [ "puiyongqing@gmail.com" ];
-        };
+    accounts.google = {
+      remote.type = "google_calendar";
+      local = {
+        type = "filesystem";
+        fileExt = ".ics";
+      };
+      khal = {
+        enable = true;
+        type = "discover";
       };
 
-      holidays = {
-        remote.type = "google_calendar";
-        local = commonLocalSettings;
-
-        khal = {
-          enable = true;
-          type = "discover";
-          readOnly = true;
-        };
-
-        vdirsyncer = commonVdirsyncerSettings // {
-          collections = [
-            "cln2srb1dhgnisr9c4hmgrrcd5i62ua0ctp6utbg5pr2sor1dhimsp31e8n6errfctm6abj3dtmg@virtual"
-          ];
-          conflictResolution = "remote wins";
+      vdirsyncer = {
+        enable = true;
+        metadata = [
+          "color"
+          "displayname"
+        ];
+        collections = [ "puiyongqing@gmail.com" ];
+        tokenFile = "${config.xdg.dataHome}/vdirsyncer/google_token";
+        clientIdCommand = [
+          "cat"
+          osConfig.sops.secrets."token/google/calendar_client_id".path
+        ];
+        clientSecretCommand = [
+          "cat"
+          osConfig.sops.secrets."token/google/calendar_client_secret".path
+        ];
+        timeRange = {
+          start = "datetime.now() - timedelta(days=365)";
+          end = "datetime.now() + timedelta(days=365)";
         };
       };
     };
   };
 
+  services.vdirsyncer.enable = true;
   programs.vdirsyncer = {
     enable = true;
     statusPath = "${config.xdg.dataHome}/vdirsyncer/status";
@@ -75,6 +46,7 @@ in
 
   programs.khal = {
     enable = true;
+    settings.default.default_calendar = "puiyongqing@gmail.com";
     locale = {
       dateformat = "%Y-%m-%d";
       timeformat = "%H:%M";
@@ -86,16 +58,5 @@ in
       firstweekday = 0;
       weeknumbers = "left";
     };
-    settings = {
-      default = {
-        default_calendar = "puiyongqing@gmail.com";
-      };
-    };
-  };
-
-  services.vdirsyncer = {
-    enable = true;
-    frequency = "hourly";
-    verbosity = "CRITICAL";
   };
 }
