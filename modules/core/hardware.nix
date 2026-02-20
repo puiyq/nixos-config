a77b5a77720a7d55a85ad46ef1ad4508fcdbdb45
Pui Yong Qing <puiyongqing@gmail.com>
2026-02-20 15:58:57 +0800

    udev: add rule for keyboard aula hero 84 he

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -5,4 +5,10 @@
     bluetooth.powerOnBoot = true;
     cpu.amd.updateMicrocode = true;
   };
+  services.udev.extraRules = ''
+    # Aula Hero 84 HE - WebUSB & WebHID
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="372e", ATTRS{idProduct}=="103e", MODE="0666", TAG+="uaccess"
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="372e", ATTRS{idProduct}=="103e", MODE="0666", TAG+="uaccess"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="372e", ATTRS{idProduct}=="103e", MODE="0666", TAG+="uaccess"
+  '';
 }
