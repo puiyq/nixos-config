39688e9d01642052dce607dce9254b94aa83068e
Pui Yong Qing <puiyongqing@gmail.com>
2026-02-28 21:57:09 +0800

    freecad-dev: update removed patch

--- overlay/hack.nix
+++ overlay/hack.nix
@@ -24,6 +24,7 @@ final: prev: {
       p:
       !(prev.lib.hasInfix "OndselSolver-pkgconfig" (toString p))
       && !(prev.lib.hasInfix "fix-font-load-crash" (toString p))
+      && !(prev.lib.hasInfix "fix-boost-189-build" (toString p))
       && !(prev.lib.hasInfix "8e04c0a3dd9435df0c2dec813b17d02f7b723b19" (toString p))
       && !(prev.lib.hasInfix "60aa5ff3730d77037ffad0c77ba96b99ef0c7df3" (toString p))
     ) oldAttrs.patches;
