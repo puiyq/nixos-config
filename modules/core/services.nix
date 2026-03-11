a29d2936fa705c0b9ef70b7423473c22c09b64f2
Pui Yong Qing <puiyongqing@gmail.com>
2026-03-11 22:47:19 +0800

    tlp: disable bt usb autosuspend in case some devices don't support it and affect connection quality

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -112,6 +112,7 @@
         SOUND_POWER_SAVE_ON_AC = 10;
         SOUND_POWER_SAVE_ON_BAT = 10;
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        USB_EXCLUDE_BTUSB = 1;
       };
     };
     pipewire = {
