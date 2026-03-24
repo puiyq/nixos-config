9ad2bb60b0ff2821cc5ecfaadf446712fb939b6b
Pui Yong Qing <puiyongqing@gmail.com>
2026-03-24 22:02:23 +0800

    mandb: disable hm generate cache

--- modules/core/documentation.nix
+++ modules/core/documentation.nix
@@ -6,5 +6,8 @@
     nixos.enable = false;
     doc.enable = false;
   };
-  home-manager.users.${username}.manual.manpages.enable = false;
+  home-manager.users.${username} = {
+    programs.man.generateCaches = false;
+    manual.manpages.enable = false;
+  };
 }
