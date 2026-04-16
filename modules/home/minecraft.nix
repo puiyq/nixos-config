639c1d1da428d250121cbc0ab6b31bcb18cfff63
Pui Yong Qing <puiyongqing@gmail.com>
2026-04-16 16:50:54 +0800

    minecraft: increase resources

--- modules/home/minecraft.nix
+++ modules/home/minecraft.nix
@@ -12,8 +12,8 @@
       BackgroundCat = "rory";
       EnableFeralGamemode = true;
       IgnoreJavaCompatibility = true;
-      MaxMemAlloc = 6 * 1024;
-      MinMemAlloc = 6 * 1024;
+      MaxMemAlloc = 8 * 1024;
+      MinMemAlloc = 8 * 1024;
       JvmArgs = "-XX:+UseZGC -XX:+UseCompactObjectHeaders -XX:+AlwaysPreTouch -XX:+UnlockExperimentalVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+DisableExplicitGC -XX:+PerfDisableSharedMem -XX:MetaspaceSize=128M -XX:MaxMetaspaceSize=512M";
     };
   };
