ead34912a8da6f8f64abc451a8c3f155355551f7
Pui Yong Qing <puiyongqing@gmail.com>
2026-04-28 13:12:42 +0800

    matlab: add container

--- modules/home/containers.nix
+++ modules/home/containers.nix
@@ -5,21 +5,21 @@
     enableTypeChecks = true;
     inherit (osConfig.virtualisation.podman) package;
     settings.storage.storage.driver = "btrfs";
-    # containers.matlab-vnc = {
-    #   image = "localhost/matlab-custom:r2025b";
-    #   autoStart = false;
-    #   ports = [
-    #     "5901:5901"
-    #     "6080:6080"
-    #   ];
-    #   volumes = [ "/home/kasumi/devshell/matlab-work:/home/matlab/work" ];
-    #   userNS = "keep-id";
-    #   extraPodmanArgs = [
-    #     "--shm-size=512M"
-    #     "--tty"
-    #     "--pull=never"
-    #   ];
-    #   exec = "-vnc";
-    # };
+    containers.matlab-vnc = {
+      image = "mathworks/matlab:r2025b";
+      autoStart = false;
+      ports = [
+        "5901:5901"
+        "6080:6080"
+      ];
+      volumes = [ "/home/kasumi/devshell/matlab-work:/home/matlab/work" ];
+      userNS = "keep-id";
+      extraPodmanArgs = [
+        "--shm-size=512M"
+        "--tty"
+        "--pull=never"
+      ];
+      exec = "-vnc";
+    };
   };
 }
