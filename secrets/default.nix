3ecbd493cba6ebcfcfac40ef38c7fe4e9da505e9
Pui Yong Qing <puiyongqing@gmail.com>
2026-05-10 19:36:59 +0800

    secrets: clean up

--- secrets/default.nix
+++ secrets/default.nix
@@ -11,49 +11,13 @@ let
     mode = "0400";
   };
 
-  wifiFile = ./networks/wifi.yaml;
-
-  certNames = [
-    "ca_cert"
-    "client_cert"
-    "private_key"
-  ];
-  wifiNetworks = {
-    eduroam = [
-      "private_key_password"
-      "identity"
-    ];
-  };
-  tokenNames = [
-    "github"
-    "wakatime"
-    "wakapi"
-    "google_calendar_client_id"
-    "google_calendar_client_secret"
-  ];
+  tokenNames = [ "github" ];
 
   dynamicSecrets = builtins.listToAttrs (
-    (map (n: {
-      name = "eduroam/${n}";
-      value = {
-        sopsFile = ./networks/certs/eduroam/${n}.pem;
-        format = "binary";
-        restartUnits = [ "iwd.service" ];
-      };
-    }) certNames)
-
-    ++ (builtins.concatMap (
-      net:
-      map (key: {
-        name = "${net}/${key}";
-        value.sopsFile = wifiFile;
-      }) wifiNetworks.${net}
-    ) (builtins.attrNames wifiNetworks))
-
-    ++ (map (n: {
+    map (n: {
       name = "token/${n}";
       value = userRO;
-    }) tokenNames)
+    }) tokenNames
   );
 in
 {
@@ -78,45 +42,6 @@ in
         owner = username;
         content = ''
           access-tokens = github.com=${config.sops.placeholder."token/github"}
-        '';
-      };
-      "wakatime" = {
-        path = "/home/${username}/.wakatime.cfg";
-        owner = username;
-        content = ''
-          [settings]
-          api_key = ${config.sops.placeholder."token/wakatime"}
-          [api_urls]
-          .* = https://wakapi.dev/api|${config.sops.placeholder."token/wakapi"}
-          .* = https://api.wakatime.com/api/v1|${config.sops.placeholder."token/wakatime"}
-        '';
-      };
-      "eduroam.8021x" = {
-        path = "/var/lib/iwd/eduroam.8021x";
-        mode = "0600";
-        owner = "root";
-        restartUnits = [ "iwd.service" ];
-        content = ''
-          [Security]
-          EAP-Method=TLS
-          EAP-Identity=${config.sops.placeholder."eduroam/identity"}
-          EAP-TLS-CACert=embed:ca_cert
-          EAP-TLS-ClientCert=embed:client_cert
-          EAP-TLS-ClientKey=embed:client_key
-          EAP-TLS-ClientKeyPassphrase=${config.sops.placeholder."eduroam/private_key_password"}
-          EAP-TLS-ServerDomainMask=radius.geteduroam.my
-
-          [Settings]
-          AutoConnect=true
-
-          [@pem@ca_cert]
-          ${config.sops.placeholder."eduroam/ca_cert"}
-
-          [@pem@client_cert]
-          ${config.sops.placeholder."eduroam/client_cert"}
-
-          [@pem@client_key]
-          ${config.sops.placeholder."eduroam/private_key"}
         '';
       };
     };
