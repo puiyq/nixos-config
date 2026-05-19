01480d736b17faa4ae0a173e79ef9b33fbe029ae
Pui Yong Qing <puiyongqing@gmail.com>
2026-05-19 17:57:30 +0800

    tailscale: remove hardcord

--- modules/core/tailscale.nix
+++ modules/core/tailscale.nix
@@ -1,8 +1,9 @@
+{config,...}:
 {
   services.tailscale = {
     enable = true;
     openFirewall = true;
   };
-  networking.firewall.trustedInterfaces = [ "tailscale0" ];
+  networking.firewall.trustedInterfaces = [ config.services.tailscale.interfaceName  ];
   systemd.services.tailscaled.serviceConfig.Environment = [ "TS_DEBUG_FIREWALL_MODE=nftables" ];
 }
