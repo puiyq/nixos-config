eac24a2ae2fa46bb7ac7938628458405e12f6c38
Pui Yong Qing <puiyongqing@gmail.com>
2026-05-22 13:41:10 +0800

    fmt

--- modules/core/tailscale.nix
+++ modules/core/tailscale.nix
@@ -1,9 +1,9 @@
-{config,...}:
+{ config, ... }:
 {
   services.tailscale = {
     enable = true;
     openFirewall = true;
   };
-  networking.firewall.trustedInterfaces = [ config.services.tailscale.interfaceName  ];
+  networking.firewall.trustedInterfaces = [ config.services.tailscale.interfaceName ];
   systemd.services.tailscaled.serviceConfig.Environment = [ "TS_DEBUG_FIREWALL_MODE=nftables" ];
 }
