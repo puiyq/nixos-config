a78cf912a5bbf5791af0775e4dc281a7e9b9321a
Pui Yong Qing <puiyongqing@gmail.com>
2026-06-15 14:03:44 +0800

    greetd: add windows vm session

--- modules/core/greetd.nix
+++ modules/core/greetd.nix
@@ -1,8 +1,60 @@
-{ inputs, username, ... }:
+{
+  inputs,
+  pkgs,
+  lib,
+  username,
+  ...
+}:
 {
   imports = [ inputs.noctalia-greeter.nixosModules.default ];
 
   programs.noctalia-greeter.enable = true;
 
   services.greetd.settings.default_session.user = username;
+
+  services.displayManager.sessionPackages =
+    let
+      mkSession =
+        id: name: text:
+        lib.extendDerivation true { providedSessions = [ id ]; } (
+          pkgs.writeTextDir "share/wayland-sessions/${id}.desktop" ''
+            [Desktop Entry]
+            Name=${name}
+            Exec=${
+              lib.getExe (
+                pkgs.writeShellApplication {
+                  name = id;
+                  inherit text;
+                }
+              )
+            }
+            Type=Application
+          ''
+        );
+    in
+    [
+      (mkSession "windows-vm" "Windows VM" ''
+        VM_NAME=win11
+
+        vm_is_running() {
+          virsh domstate "$VM_NAME" 2>/dev/null | grep -q "^running"
+        }
+
+        cleanup() {
+          if vm_is_running; then
+            virsh shutdown "$VM_NAME"
+          fi
+          systemctl start beesd@root
+        }
+        trap cleanup EXIT
+
+        systemctl stop beesd@root
+
+        if ! vm_is_running; then
+          virsh start "$VM_NAME"
+        fi
+
+        cage -- virt-viewer --attach --connect qemu:///system "$VM_NAME"
+      '')
+    ];
 }
