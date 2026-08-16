47cc43a355e19ffbf9dffbf121e6d69e730aff10
Pui Yong Qing <puiyongqing@gmail.com>
2026-08-17 04:54:34 +0800

    ollama: remove workaround

--- modules/core/ollama.nix
+++ modules/core/ollama.nix
@@ -14,5 +14,4 @@
       package = if config.drivers.amdgpu.rocm.enable then pkgs.ollama-rocm else pkgs.ollama-vulkan;
     };
   };
-  boot.kernelModules = [ "amdgpu" ]; # workaround of race condition (see issue #422355 on nixpkgs)
 }
