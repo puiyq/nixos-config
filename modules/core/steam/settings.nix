80678a36a30b443fc80e6fe5950843722ad2d348
Pui Yong Qing <puiyongqing@gmail.com>
2026-08-17 04:54:34 +0800

    steam: refactor config

--- modules/core/steam/settings.nix
+++ modules/core/steam/settings.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   lib,
+  host,
   username,
   ...
 }:
@@ -12,55 +13,65 @@
         export PROTON_DXVK_LOWLATENCY=1
       '';
     };
-    config =
-      let
-        niri = lib.getExe pkgs.niri;
-      in
-      {
-        enable = true;
-        onSteamRunning = "close";
-        defaultCompatTool = pkgs.dwproton-bin;
+    config = {
+      enable = true;
+      onSteamRunning = "close";
+      defaultCompatTool = pkgs.dwproton-bin;
 
-        apps = {
-          "Malody V" = {
-            id = 1512940;
-            compatTool = pkgs.dwproton-bin;
-            preHook = ''
-              ${niri} msg output DP-1 mode 2560x1440@199.997
-              ${niri} msg output DP-1 vrr no
-            '';
-            env = {
-              PROTON_ENABLE_WAYLAND = true;
-              PROTON_USE_WOW64 = true;
-            };
-          };
-          "Cyberpunk 2077" = {
-            id = 1091500;
-            compatTool = pkgs.dwproton-bin;
-            preHook = ''
-              ${niri} msg output DP-1 mode 2560x1440@119.998
-              ${niri} msg output DP-1 vrr yes
-            '';
-            wrappers = [ (lib.getExe pkgs.gamemode) ];
-            env = {
-              WINEDLLOVERRIDES = "winmm,version=n,b";
-              PROTON_ENABLE_WAYLAND = true;
-              PROTON_USE_WOW64 = true;
-              PROTON_FSR4_UPGRADE = true;
-              PROTON_MLFG_UPGRADE = true;
-              DXVK_HDR = true;
-            };
-            args = [ "--launcher-skip" ];
+      apps = {
+        "Malody V" = {
+          id = 1512940;
+          compatTool = pkgs.dwproton-bin;
+          env = {
+            PROTON_ENABLE_WAYLAND = true;
+            PROTON_USE_WOW64 = true;
           };
         };
-        nonSteamApps = {
+        "Cyberpunk 2077" = {
+          id = 1091500;
+          compatTool = pkgs.dwproton-bin;
+          wrappers = [ (lib.getExe pkgs.gamemode) ];
+          env = {
+            PROTON_ENABLE_WAYLAND = true;
+            PROTON_USE_WOW64 = true;
+            PROTON_FSR4_UPGRADE = true;
+            PROTON_MLFG_UPGRADE = true;
+            DXVK_HDR = true;
+          };
+          args = [ "--launcher-skip" ];
+          dllOverrides = {
+            winmm = "n,b";
+            version = "n,b";
+          };
+        };
+        "The Elder Scrolls V: Skyrim" = {
+          id = 489830;
+          compatTool = pkgs.dwproton-bin;
+          wrappers = [ (lib.getExe pkgs.gamemode) ];
+          env = {
+            PROTON_ENABLE_WAYLAND = true;
+            PROTON_USE_WOW64 = true;
+            PROTON_FSR4_UPGRADE = true;
+            PROTON_MLFG_UPGRADE = true;
+            PROTON_USE_OPTISCALER = true;
+            DXVK_HDR = true;
+          };
+          dllOverrides = {
+            winmm = "n,b";
+            version = "n,b";
+          };
+        };
+      };
+      nonSteamApps = (
+        lib.mkIf (host == "popipa") {
           "White Album 2" = {
             id = 2889710772;
             allowOverlay = false;
             compatTool = pkgs.dwproton-bin;
             target = "/home/${username}/Documents/White Album 2/WA2_chs.exe";
           };
-        };
-      };
+        }
+      );
+    };
   };
 }
