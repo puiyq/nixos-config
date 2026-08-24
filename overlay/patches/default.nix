bcfd8ba9fa1598545f0a69fa04a12868fb49bb66
Pui Yong Qing <puiyongqing@gmail.com>
2026-08-24 20:57:14 +0800

    ananicy-cpp: remove patch

--- overlay/patches/default.nix
+++ overlay/patches/default.nix
@@ -1,11 +1,1 @@
-_final: prev: {
-  ananicy-cpp = prev.ananicy-cpp.overrideAttrs {
-    patches = [
-      (prev.fetchpatch2 {
-        name = "fix-cstring-include.patch";
-        url = "https://gitlab.com/ananicy-cpp/ananicy-cpp/-/merge_requests/43.diff";
-        hash = "sha256-6J7dOunqoa8umCDW6mX28HbL/cn+aaOCcLyhgZBrVX4=";
-      })
-    ];
-  };
-}
+_final: _prev: { }
