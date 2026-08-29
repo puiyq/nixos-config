94af76882011722c697072948bc894e8d14cb020
Pui Yong Qing <puiyongqing@gmail.com>
2026-08-29 09:39:18 +0800

    Revert "boot: remove kernel module that failed to build"
    This reverts commit 9a6ba2f211872647fd84d962e1427c5ad4720dba.

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  config,
   lib,
   host,
   ...
@@ -7,6 +8,10 @@
 {
   boot = {
     kernelPackages = pkgs.linuxPackages_cachyos-lto-znver4;
+
+    extraModulePackages = with config.boot.kernelPackages; [
+      (lib.mkIf (host == "roselia") nct6687d)
+    ];
 
     kernelParams = [
       # disable startup log
